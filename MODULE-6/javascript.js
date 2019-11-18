1ed99fbdf2e6e602e6482760318b507235d95927
Jakeokony1024 <jakeokony@gmail.com>
2019-11-17 19:39:42 -0600

    Create javascript.js

---
+++ MODULE-6/javascript.js
@@ -0,0 +1,16 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "300px"; 
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+    
+    document.getElementById("box").style.height = "0px";
+
+});
